01609cee05713c61824dcfaf50321e243b36fbf5
Miura <hiroyuki.miura@daimler.com>
2019-04-22 00:25:33 +0900

    Minor correction in texts.

--- script.js
+++ script.js
@@ -6,22 +6,24 @@
 $('document').ready(function () {
     "use strict";
     //Script Here
+
+    //Tab UI, move selected tab
     $('.tab-item > a').click(function () {
         $('.tab-item').removeClass('isSelected');
         $(this).parent().addClass('isSelected');
     });
 
+    //Automatically hide header on scroll
+    //Hide Header on on scroll down
     var startPos = 0,
         winScrollTop = 0;
     $(window).on('scroll', function () {
         winScrollTop = $(this).scrollTop();
-        if (winScrollTop >= startPos) {
+        if (winScrollTop > startPos) {
             $('header').addClass('hide');
         } else {
             $('header').removeClass('hide');
         }
         startPos = winScrollTop;
     });
-
-
 });
