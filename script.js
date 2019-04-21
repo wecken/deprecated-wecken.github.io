5784e55d2d2d2bf17585ea368ebe9269afdc99f9
Miura <hiroyuki.miura@daimler.com>
2019-04-21 22:10:03 +0900

    Added script to show and hide header on scroll. Created glass effect with backdrop filter, works only when browser supports.

--- script.js
+++ script.js
@@ -10,4 +10,18 @@ $('document').ready(function () {
         $('.tab-item').removeClass('isSelected');
         $(this).parent().addClass('isSelected');
     });
+
+    var startPos = 0,
+        winScrollTop = 0;
+    $(window).on('scroll', function () {
+        winScrollTop = $(this).scrollTop();
+        if (winScrollTop >= startPos) {
+            $('header').addClass('hide');
+        } else {
+            $('header').removeClass('hide');
+        }
+        startPos = winScrollTop;
+    });
+
+
 });
