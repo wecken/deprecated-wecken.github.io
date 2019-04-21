353987385c67822c7c76db8c093a93d444de21df
Miura <hiroyuki.miura@daimler.com>
2019-04-21 12:00:42 +0900

    stable layout, still detail work needed.

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
 //JS
 $('document').ready(function () {
     "use strict";
-    for (;;) {
-        window.alert("∧_∧　ババババ\n（ ・ω・)=つ≡つ\n（っ ≡つ=つ\n`/　　)\n(ノΠＵ\n何回閉じても無駄ですよ～ww\nm9（＾Д＾）プギャー！！");
-    }
+    // 何かしらの処理
+    
 });
