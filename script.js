d19f644e0d0775a8a1294f0e9a4f1adfe5b87fb5
Wecken <wecken@WeckenMBP.local>
2019-08-06 22:12:36 +0900

    Added script to make clicked image full screen.

--- script.js
+++ script.js
@@ -70,6 +70,25 @@ $('document').ready(function () {
                 return flug;
             }, 700);
         }
+
+    });
+
+    //Show image in full-screen on click.
+    $('img[data-enlargable]').addClass('img-enlargable').click(function () {
+        var src = $(this).attr('src');
+        $('<div>').css({
+            background: 'RGBA(0,0,0,.8) url(' + src + ') no-repeat center',
+            backgroundSize: 'contain',
+            width: '100%',
+            height: '100%',
+            position: 'fixed',
+            zIndex: '10000',
+            top: '0',
+            left: '0',
+            cursor: 'zoom-out'
+        }).click(function () {
+            $(this).remove();
+        }).appendTo('body');
     });
 
 });
