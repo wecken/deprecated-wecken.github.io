75e677e4e9741b39ecf981bd88416d1820abde07
Wecken <wecken@WeckenMBP.local>
2019-08-12 00:11:46 +0900

    Refactored JS for making image full screen. Added animation for fading in full screen image

--- script.js
+++ script.js
@@ -74,21 +74,12 @@ $('document').ready(function () {
     });
 
     //Show image in full-screen on click.
-    $('img[data-enlargable]').addClass('img-enlargable').click(function () {
-        var src = $(this).attr('src');
-        $('<div>').css({
-            background: 'RGBA(0,0,0,.8) url(' + src + ') no-repeat center',
-            backgroundSize: 'contain',
-            width: '100%',
-            height: '100%',
-            position: 'fixed',
-            zIndex: '10000',
-            top: '0',
-            left: '0',
-            cursor: 'zoom-out'
-        }).click(function () {
-            $(this).remove();
-        }).appendTo('body');
+    $('img[data-enlargable]').click(function () {
+        var url = "url" + "(" + $(this).attr('src') + ")";
+        $('#full-screen-image').addClass('isFullScreen');
+        $('#full-screen-image').css('background-image', url);
     });
-
+    $('#full-screen-image').click(function () {
+        $(this).removeClass('isFullScreen');
+    });
 });
